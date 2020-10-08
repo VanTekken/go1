22f905109f119cb3a0fa31cdcf4a0ae879b9bb39
Harry <harry.crawford789@hotmail.co.uk>
2020-10-08 10:45:49 +0100

    Page data structure and save method

--- wiki.go
+++ wiki.go
@@ -4,3 +4,16 @@ import (
 	"fmt"
 	"io/ioutil"
 )
+
+//Page data structure
+type Page struct{
+	Title string
+	//byte slice
+	Body []byte
+}
+
+//save method
+func (p *Page) save() error{
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
