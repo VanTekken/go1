6744ac08a52d1677f5769f69e0802822c6be8ee3
Harry Cee <harry.crawford789@hotmail.co.uk>
2021-01-27 17:37:08 +0000

    started building default page for when nothing exists in a fresh install

--- wiki.go
+++ wiki.go
@@ -8,31 +8,28 @@ import (
 	"html/template"
 )
 
-//Page data structure
+//In-Memory Storage
 type Page struct{
 	Title string
-	//byte slice
 	Body []byte
 }
 
-//save method
+//Persistent Storage
 func (p *Page) save() error{
 	filename := p.Title + ".html"
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
-
 func loadPage(title string) (*Page, error){
-	filename := title + ".html"
-	//underscore used to throw away return value
+    filename := "templates/" + title + ".html"
 	body, err := ioutil.ReadFile(filename)
 	if err != nil{
 		return nil, err
-	}
-	return &Page{Title: title, Body: body}, nil
+    }
+    return &Page{Title: title, Body: body}, nil
 }
-
+//Render html template file
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-    t, err := template.ParseFiles(tmpl + ".html")
+    t, err := template.ParseFiles("templates/" + tmpl + ".html")
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
@@ -41,27 +38,39 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
     }
+
 }
 
+//MVC Router
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+    //Redirect root dir requests to the view page
+    http.Redirect(w, r, "/view/",http.StatusFound)
+}
 func viewHandler(w http.ResponseWriter, r *http.Request) {
     title := r.URL.Path[len("/view/"):]
     p, err := loadPage(title)
-    if err != nil {
-        http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+    if p == nil{
+        renderTemplate(w, "welcome", p)
+    }else if err != nil{
+        http.Redirect(w, r, "/error/",http.StatusFound)
         return
+    }else{
+        renderTemplate(w, "view", p)
     }
-    renderTemplate(w, "view", p)
+    
 }
-
 func editHandler(w http.ResponseWriter, r *http.Request) {
     title := r.URL.Path[len("/edit/"):]
     p, err := loadPage(title)
     if err != nil {
         p = &Page{Title: title}
     }
-    renderTemplate(w, "edit", p)
+    if title == "" {
+        renderTemplate(w, "edit-help", p)
+    }else{
+        renderTemplate(w, "edit", p)
+    }
 }
-
 func saveHandler(w http.ResponseWriter, r *http.Request) {
     title := r.URL.Path[len("/save/"):]
     body := r.FormValue("body")
@@ -75,9 +84,10 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main(){
+    http.HandleFunc("/", homeHandler)
+    http.HandleFunc("/welcome", viewHandler)
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
 	http.HandleFunc("/save/", saveHandler)
 	log.Fatal(http.ListenAndServe(":8080",nil))
 }
-
